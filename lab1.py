92e684795267601aa5d82a926ebdd0887cbcf0c9
wenhan6 <wenhan6@ualberta.ca>
2023-09-13 18:52:34 -0600

    new changes

--- lab1.py
+++ lab1.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/wenhan6/lab1/main/lab1.py")
 
 print(resp.text)
